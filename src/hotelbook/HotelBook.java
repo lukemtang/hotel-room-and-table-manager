cf1b6308d07acd45af14e89b31e68bdcbd5c414c
Xylobone <lukemanntang@gmail.com>
2022-07-07 15:36:02 +0100

    Add first text fields and buttons

--- src/hotelbook/HotelBook.java
+++
@@ -1,7 +0,0 @@
-package hotelbook;
-
-public class HotelBook {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
-}
--- src/hotelbook/InputScreen.java
+++ src/hotelbook/InputScreen.java
@@ -5,11 +5,29 @@ public class InputScreen {
   public static void main(String args[]){
     JFrame myFrame = new JFrame("Hotel Booker");
     // Setting the size of the myFrame
-    myFrame.setSize(1000, 500);
+    myFrame.setSize(400, 250);
     myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    JLabel myLabel = new JLabel("TEST");
+    JLabel myLabel = new JLabel("Welcome to the hotel room booking service.");
+    myLabel.setBounds(50, 20,400, 30 );
     myFrame.add(myLabel);
+
+
+    // Adding two text fields to get guest name and any details
+    JTextField tField1 = new JTextField("Enter Guest name");
+    tField1.setBounds(50, 60, 200,30);
+    myFrame.add(tField1);
+    JTextField tField2 = new JTextField("Enter Guest stay details");
+    tField2.setBounds(50,110, 200,30);
+    myFrame.add(tField2);
+
+    // Save button
+    JButton button1 = new JButton("Post details");
+    button1.setBounds(50, 160, 150, 25);
+    myFrame.add(button1);
+
+    myFrame.setLayout(null);
     myFrame.setVisible(true);
+
   }
 
 }
